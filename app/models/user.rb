93084e330188f1233c8338e6a03d091d49f716af
dthaug <daniel_haugland@hotmail.com>
2013-07-31 00:19:17 +0200

    Fixed a little on DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         #:recoverable,
+        :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   
   has_many :evaluations, class_name: "RSEvaluation", as: :source
   has_reputation :votes, source: {reputation: :votes, of: :dilemmas}, aggregated_by: :sum
@@ -11,15 +12,6 @@ class User < ActiveRecord::Base
   has_many :dilemmas
 
   validates :name, presence: true, uniqueness: true
-
-  validates_attachment :avatar,
-                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-                            size: { less_than: 5.megabytes }
-
-  has_attached_file :avatar, styles: { thumb: "38x38",
-                                      medium: "300x300#",
-                                      big: "750x750#"},
-                                      :default_url => '/images/missing_style.jpg'
 
   
     def voted_for?(dilemma)
--- db/schema.rb
+++ db/schema.rb
@@ -90,10 +90,6 @@ ActiveRecord::Schema.define(:version => 20130730182438) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
