57ab96acee95aa53d5a5545d209f908dce38b85f
dthaug <daniel_haugland@hotmail.com>
2013-07-31 00:27:46 +0200

    Gitlock delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Rether::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'fathomless-coast-6021.heroku.com' }
+  
 end
