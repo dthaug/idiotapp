1500280cfcfd731477040f69ad49cf196c4ed298
dthaug <daniel_haugland@hotmail.com>
2013-07-31 03:27:01 +0200

    Added lots of css stuff, put in validation for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ActiveRecord::Base
   attr_accessible :description, :votes, :dilemma_id
   belongs_to :dilemma
 
-  validates :description, presence: true
+  validates :description, presence: true,
+                 :length => { :in => 4..160 }
 
   has_reputation :votes, source: :user, aggregated_by: :sum
 
